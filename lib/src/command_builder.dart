6b0d2bb746ca04839e078ac432229f8e110bfedf
pierremrtn <pierre2tm@gmail.com>
2025-01-08 09:21:34 +0700

    improved command api

--- lib/src/command_builder.dart
+++ lib/src/command_builder.dart
@@ -3,19 +3,39 @@ import 'package:flutter/widgets.dart';
 import 'command.dart';
 import 'result.dart';
 
-class CommandBuilder<T> extends StatelessWidget {
-  const CommandBuilder({
+abstract class CommandBuilder<T> extends StatelessWidget {
+  const factory CommandBuilder.result({
+    Key key,
+    required Command<T> command,
+    required Widget Function(BuildContext context, T value) success,
+    required Widget Function(BuildContext context, Object error) failure,
+    required Widget Function(BuildContext context) orElse,
+  }) = _CommandBuilderResult<T>;
+
+  const factory CommandBuilder.status({
+    Key key,
+    required Command<T> command,
+    required Widget Function(BuildContext context) initial,
+    required Widget Function(BuildContext context, T value) success,
+    required Widget Function(BuildContext context, Object error) failure,
+    required Widget Function(BuildContext context) running,
+  }) = _CommandBuilderStatus<T>;
+}
+
+class _CommandBuilderResult<T> extends StatelessWidget
+    implements CommandBuilder<T> {
+  const _CommandBuilderResult({
     super.key,
     required this.command,
     required this.success,
     required this.failure,
-    required this.loading,
+    required this.orElse,
   });
 
   final Command<T> command;
   final Widget Function(BuildContext context, T value) success;
   final Widget Function(BuildContext context, Object error) failure;
-  final Widget Function(BuildContext context) loading;
+  final Widget Function(BuildContext context) orElse;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +44,38 @@ class CommandBuilder<T> extends StatelessWidget {
       builder: (context, _) => switch (command.result) {
         Success(:final value) => success(context, value),
         Failure(:final error) => failure(context, error),
-        null => loading(context),
+        null => orElse(context),
+      },
+    );
+  }
+}
+
+class _CommandBuilderStatus<T> extends StatelessWidget
+    implements CommandBuilder<T> {
+  const _CommandBuilderStatus({
+    super.key,
+    required this.command,
+    required this.initial,
+    required this.success,
+    required this.failure,
+    required this.running,
+  });
+
+  final Command<T> command;
+  final Widget Function(BuildContext context) initial;
+  final Widget Function(BuildContext context, T value) success;
+  final Widget Function(BuildContext context, Object error) failure;
+  final Widget Function(BuildContext context) running;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: command,
+      builder: (context, _) => switch (command.status) {
+        CommandStatus.initial => initial(context),
+        CommandStatus.success => success(context, command.value as T),
+        CommandStatus.failure => failure(context, command.error!),
+        CommandStatus.running => running(context),
       },
     );
   }
